76647ef5de5f2074cbebe5b82d03eb55e9dda2e4
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-10-25 22:29:55 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const sequelize = require('./config/connection');
+const routes = require('./controllers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+const hbs = exphbs.create({});
+
+const sess = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
+app.use(session(sess));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+sequelize.sync({force:false}).then(() =>{
+    app.listen(PORT, () => console.log(`Now listening on port ${PORT}`));
+});
